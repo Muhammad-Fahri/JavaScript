909e05766ebf7896af5619ad7facb91c46fd4ba3
Muhammad-Fahri <mhdfahri224@gmail.com>
2021-12-31 11:37:52 +0700

    Project Pertama

---
+++ Core Objects.js
@@ -0,0 +1,31 @@
+Given the array below, please complete the expression to be alerted with "apple".
+var fruits = new Array("pear", "orange",
+"apple", "grapefruit");
+alert(fruits
+[2]
+);
+
+
+
+What is the result of the following expression?
+alert(Math.sqrt(36));
+ 6
+
+
+Please fill in the blanks to output the current minutes:
+ var date = new Date();
+ alert(date.getMinutes());
+
+
+What is the output of this code?
+var arr = new Array("a", "b", "c"); 
+alert(arr[1]);
+b 
+
+
+Drag and drop from the options below to get alerted with the value of the PI constant.
+alert(math.PI);
+
+
+
+
---
+++ Objects.js
@@ -0,0 +1,15 @@
+An object's properties are similar to variables; methods are similar to:
+  functions
+
+
+What is the result of the following expression?
+var myString = "abcdef";
+document.write(myString.length);
+ 6
+
+
+Complete the expression to create an object constructor, taking into account that "height" and "weight" are properties and "calculate" is a method for the given object:
+  function mathCalc (height, weight) {this.height = height;
+    this.weight = weight;
+    this.sampleCalc = calculate;
+  }
